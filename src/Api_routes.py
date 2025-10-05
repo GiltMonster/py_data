4e1274f29c541d8a4892713a05949639f5e3e092
GiltMonster <lucassantoslukas2@gmail.com>
2025-10-05 18:25:33 -0300

    feat ✨: Adiciona template HTML para a página inicial e corrige a inicialização do Flask na classe Api_routes

--- src/Api_routes.py
+++ src/Api_routes.py
@@ -1,9 +1,9 @@
-class Api_routes:
-    from flask import Flask
+from flask import Flask, render_template
 
+class Api_routes:
     def __init__(self, app=None):
         if app is None:
-            self.app = self.Flask(__name__)
+            self.app = Flask(__name__)
         else:
             self.app = app
         self.setup_routes()
@@ -11,12 +11,11 @@ class Api_routes:
     def setup_routes(self):
         @self.app.route('/')
         def home():
-            return "Welcome to the API!"
+            return render_template('index.html')
         
         @self.app.route('/status')
         def status():
             return {"status": "running"}
 
-    
     def run(self, debug=True, host='0.0.0.0', port=3000):
         self.app.run(debug=debug, host=host, port=port)
